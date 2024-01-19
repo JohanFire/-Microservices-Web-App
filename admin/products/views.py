0862ad31ab886dcaf531950b01495a6b7a0a60fc
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2024-01-19 13:37:18 -0600

    class User and get random User

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,7 +1,10 @@
+import random
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ViewSet):
@@ -43,3 +46,10 @@ class ProductViewSet(viewsets.ViewSet):
         product.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+
+        return Response({'id': user.id})
